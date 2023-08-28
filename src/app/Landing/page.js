7d3c905a42ed3040e26e1d0a7eb66f75551041c6
bovdaboat <ignatgabov@gmail.com>
2023-08-27 18:28:56 -0700

    added the link so i scroll to differnt pages

--- src/app/Landing/page.js
+++ src/app/Landing/page.js
@@ -39,10 +39,10 @@ const Landing = () => {
                     <div className="w-full navbar bg-green-800">
                         <div className="flex-none lg:hidden">
                             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current bg-white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
                         </div>
-                        <div className="flex-1 px-2 mx-2 text-2xl font-mono">Stella_TsuNami</div>
+                        <div className="flex-1 px-2 mx-2 text-2xl font-mono text-white">Stella_TsuNami</div>
 
                         <div>
                             <label className="swap swap-rotate m-3">
@@ -73,8 +73,8 @@ const Landing = () => {
                         <div className="flex-none hidden lg:block">
                             <ul className="menu menu-horizontal">
                                 {/* Page menu content here */}
-                                <li><Link href="#Collection"  className="font-mono">Collection</Link></li>
-                                <li><Link href="#Contact" className="font-mono">Contact</Link></li>
+                                <li><Link href="#Collection"  className="font-mono text-white">Collection</Link></li>
+                                <li><Link href="#Contact" className="font-mono text-white">Contact</Link></li>
                             </ul>
                         </div>
                     </div>
